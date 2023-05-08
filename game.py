c4ca52a52a03a3901e130b73b2a834b16cedc781
Janagan-Ra <47286607+Janagan-Ra@users.noreply.github.com>
2023-05-08 18:28:41 -0400

    Drawn board
    Grid has been made but 0 functionality

--- game.py
+++ game.py
@@ -1,22 +1,39 @@
 # GUI.py
 import pygame
-pygame.font.init()
 
 
 class sudoku:
-    def __init__ (self, rows, collums, display):
+    def __init__ (self, rows, collums, size, display):
         self.rows = rows
         self.collums = collums
+        self.size = size
+        self.model = None
+        self.display = display
+    
 
 
-
+width = 800
+hight = 800
 
 def main():
-    display = pygame.display.set_mode((800,800))
+    pygame.init()
+    display = pygame.display.set_mode((width,hight))
     pygame.display.set_caption("Sudoku")
-    board = sudoku(9, 9,display)
-    key = None
-    run = True
-  
+    display.fill((220,220,220))
+    
+    #grid
+    for i in range(0,10):
+        pygame.draw.line(display, (0,0,0), ((width/16) + (width/16)*i, (width/16)), ((hight/16) + (hight/16)*i, 500), 2)
+        pygame.draw.line(display, (0,0,0), ((width/16), (width/16) + (width/16)*i), (500, (hight/16) + (hight/16)*i), 2)
+        if (i % 3 == 0):
+            pygame.draw.line(display, (0,0,0), ((width/16) + (width/16)*i, (width/16)), ((hight/16) + (hight/16)*i, 500), 5)
+            pygame.draw.line(display, (0,0,0), ((width/16), (width/16) + (width/16)*i), (500, (hight/16) + (hight/16)*i), 5)
+        pygame.display.update()
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
 main()
-pygame.quit()
