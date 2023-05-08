111bf9a95ce9f1b52e094d519fbc105e3b4fd771
Janagan-Ra <47286607+Janagan-Ra@users.noreply.github.com>
2023-05-08 18:49:10 -0400

    display text
    render board but no font

--- game.py
+++ game.py
@@ -16,7 +16,20 @@ width = 800
 hight = 800
 
 def main():
+    board = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
     pygame.init()
+    font = pygame.font.SysFont('arial',30)
     display = pygame.display.set_mode((width,hight))
     pygame.display.set_caption("Sudoku")
     display.fill((220,220,220))
@@ -29,6 +42,12 @@ def main():
             pygame.draw.line(display, (0,0,0), ((width/16) + (width/16)*i, (width/16)), ((hight/16) + (hight/16)*i, 500), 5)
             pygame.draw.line(display, (0,0,0), ((width/16), (width/16) + (width/16)*i), (500, (hight/16) + (hight/16)*i), 5)
         pygame.display.update()
+
+    for i in range(9):
+        for j in range(9):
+            value = font.render(str(board[i][j]),True,(0,200,0))
+            display.blit(value,((j+1)*50 + 50, (i+1)*50 + 50))
+
     
     while True:
         for event in pygame.event.get():
